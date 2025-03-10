7e052bb6c4b15c58969d4f61945462358ef25311
Hafeezur Rahman <hafeezofficial08@gmail.com>
2025-03-11 00:40:49 +0530

    New Version

--- src/style.js
+++ src/style.js
@@ -1,9 +1,18 @@
-const menu = document.getElementById("menu");
-        const mobileMenu = document.getElementById("mobile-menu");
-        menu.addEventListener("click", function(){
-            if (mobileMenu.style.display === "flex") {
-               mobileMenu.style.display = "none";  // Hide the menu
-            } else {
-               mobileMenu.style.display = "flex";  // Show the menu
-            }   
-})
+// const menu = document.getElementById("menu");
+//         const mobileMenu = document.getElementById("mobile-menu");
+//         menu.addEventListener("click", function(){
+//             if (mobileMenu.style.display === "flex") {
+//                mobileMenu.style.display = "none";  // Hide the menu
+//             } else {
+//                mobileMenu.style.display = "flex";  // Show the menu
+//             }   
+// })
+
+
+
+
+function toggleMenu() {
+        const nav = document.querySelector('nav');
+        nav.classList.toggle('expanded');
+    }
+
